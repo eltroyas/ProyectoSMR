dee3cb604dde8a8ca249dc743b4b626d9c50f1a4
Iván <ivangimeno@proton.me>
2026-01-25 21:33:02 +0100

    update

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -18,7 +18,7 @@ exports.handler = async function(event, context) {
       : `Responde de la mejor manera posible a la pregunta: ${message}`;
 
     const HF_TOKEN = process.env.HF_TOKEN;
-    const MODEL = "Qwen/Qwen-3-1.7B";
+    const MODEL = "Qwen/Qwen3-4B-Instruct-2507";
     const HF_URL = `https://router.huggingface.co/models/${MODEL}`;
 
     // Timeout de 9 segundos
